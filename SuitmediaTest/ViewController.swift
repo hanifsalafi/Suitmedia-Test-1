e9ef9937c26edba283f382e2de6fae5f9f902fd4
hanifsalafi <hanifsalafi456@gmail.com>
2021-05-23 13:29:09 +0700

    Initial Commit

---
+++ SuitmediaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuitmediaTest
+//
+//  Created by MacBook on 23/05/21.
+//  Copyright © 2021 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
